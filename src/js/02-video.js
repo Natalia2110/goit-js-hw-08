adeac1b00f800ab23d63edf00e7aabf5b8cc8d39
Natalia Yakovyna <yakovina.n13@gmail.com>
2023-05-13 14:46:25 +0300

    task02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'; 
 
+
+const player = new Player('handstick', {
+    id: 19231868,
+    width: 640
+});
+
+player.on('play', function() {
+    console.log('played the video!');
+});
